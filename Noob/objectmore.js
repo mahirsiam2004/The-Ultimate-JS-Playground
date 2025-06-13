c545940d3d67c3624f39b3062fe0634b2567d0bb
M_FOR_MAHIR <mahirsiam2004@gmail.com>
2025-06-13 14:29:06 +0600

    added string in js

---
+++ Noob/objectmore.js
@@ -0,0 +1,54 @@
+const computer = {
+  brand: "lenovo",
+  price: 35000,
+  processor: "intel",
+  ssd: "512gb",
+};
+
+for (const m in computer) {
+  console.log(m);
+  console.log(computer[m]);
+}
+
+const newObject = new Object();
+const newObject2 = Object.create();
+colsole.log(newObject);
+colsole.log(newObject2);
+
+const person = { name: "Alice", age: 25, country: "Bangladesh" };
+delete person.age;
+console.log(person);
+
+// Output: { name: 'Alice', country: 'Bangladesh' }
+
+const adminUser = {
+  username: "admin",
+  email: "admin@example.com",
+  role: "superadmin",
+};
+
+Object.freeze(adminUser); // Attempt to modify the object
+
+adminUser.role = "user";
+adminUser.password = "123456";
+delete adminUser.email;
+
+console.log(adminUser);
+
+// Output: {username: 'admin', email: 'admin@example.com', role: 'superadmin'}
+
+const user = {
+  username: "johndoe",
+  email: "john.doe@example.com",
+  password: "oldpassword",
+};
+
+Object.seal(user); // Attempt to modify the object
+
+user.password = "newpassword"; // Allowed
+user.age = 30; // adding new property is not allowed
+delete user.email; // deleting property is not allowed
+
+console.log(user);
+
+// Output: {username: 'johndoe', email: 'john.doe@example.com', password: 'newpassword'}
---
+++ Noob/string.js
@@ -0,0 +1,16 @@
+const name = "Rafiq";
+const age = 30;
+console.log("Name: " + name + ", Age: " + age);
+console.log(`Name: ${name}, Age: ${age}`);
+
+// Output:
+// Name: Rafiq, Age: 30
+// Name: Rafiq, Age: 30
+
+const getName = function () {
+  return "Nadia";
+};
+const message = `Na Bolle Sadia, ase amar ${getName()}!`;
+console.log(message);
+
+// Output: Na Bolle Sadia, ase amar Nadia!
