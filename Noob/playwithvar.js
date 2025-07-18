dbcbf3a0036b765cd85369239a0b9ddd0f14752b
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-07-18 15:57:25 +0600

    added more

---
+++ Noob/playwithvar.js
@@ -0,0 +1,9 @@
+let names=['mahir','siam','riyad','shafayet','shawon'];
+
+let index=-1;
+let name=names[++index];
+
+setInterval(()=>{
+    name=names[index++];
+    console.log(name, name.length);
+},1000);
