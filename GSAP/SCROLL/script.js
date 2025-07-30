dde27914aa95d6b279d2a6ea7e42dfbfe4da7b7e
mahirsiam2004 <mahirsiam2004@gmail.com>
2025-07-30 22:27:27 +0600

    added unfinished scrolleffect

--- GSAP/SCROLL/script.js
+++ GSAP/SCROLL/script.js
@@ -1,19 +1,56 @@
-gsap.from("#page-1 #box",{
+// gsap.from("#page-1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360
+// })
+// gsap.from("#page-2 #box",{
+//     scale:0,
+//     // delay:1,
+//     duration:2,
+//     rotate:360,
+//     // scrollTrigger:"page-2 #box"
+//     scrollTrigger:{
+//         trigger:"#page-2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
+
+gsap.from("#page-2 h1", {
+  opacity: 0,
+  duration: 2,
+  x: 500,
+  scrollTrigger: {
+    trigger: "#page-2 h1",
+    markers: true,
+    start: "top 50%",
+  },
+});
+
+gsap.from("#page-2 h2", {
+  opacity: 0,
+  duration: 2,
+  x: -500,
+  scrollTrigger: {
+    trigger: "#page-2 h2",
+    markers: true,
+    start: "top 50%",
+  },
+});
+gsap.from("#page-1 #box", {
     scale:0,
-    delay:1,
-    duration:2,
-    rotate:360
-})
-gsap.from("#page-2 #box",{
-    scale:0,
-    // delay:1,
-    duration:2,
-    rotate:360,
-    // scrollTrigger:"page-2 #box"
-    scrollTrigger:{
-        trigger:"#page-2 #box",
-        scroller:"body",
-        markers:true,
-        start:"top 60%"
-    }
-})
+  opacity: 0,
+  rotate:720,
+  duration: 1,
+  scrollTrigger: {
+    trigger: "#page-1 #box",
+    scroller:"body",
+    markers: true,
+    start: "top 60%",
+    // end:"top 10%",
+    scrub:2,
+    pin:true
+  },
+});
